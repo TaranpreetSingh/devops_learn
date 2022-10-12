566e5dfe3b630864f139985aadb435d897f11715
singh20taran@gmail.com <singh20taran@gmail.com>
2022-10-12 19:21:59 +0530

    lb_oci_block

--- terraform/oracle/dns/main.tf
+++ terraform/oracle/dns/main.tf
@@ -1,3 +1,8 @@
+data "oci_load_balancer_load_balancers" "test_load_balancers" {
+    #Required
+    compartment_id = var.compartment_id
+
+
 data "oci_load_balancer_hostnames" "test_hostnames" {
     #Required
     load_balancer_id = oci_load_balancer_load_balancer.test_load_balancer.id
